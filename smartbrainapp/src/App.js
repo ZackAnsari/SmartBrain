e1df47fd41ddb78219cb0d41edbd40d9092e7756
Zack Ansari <zackansari1@gmail.com>
2023-11-13 21:32:51 -0500

    changed backend url

--- smartbrainapp/src/App.js
+++ smartbrainapp/src/App.js
@@ -117,7 +117,7 @@ class App extends Component {
       .then((response) => {
         // console.log("hi", response.outputs[0].data.regions[0].region_info.bounding_box);
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smartbrain-backend-inqh.onrender.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- smartbrainapp/src/components/Signin/Signin.js
+++ smartbrainapp/src/components/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component {
         this.setState({signInPassword: event.target.value})
     }
     onSubmitSignIn = () =>{
-        fetch("http://localhost:3000/signin",{
+        fetch("https://smartbrain-backend-inqh.onrender.com/signin",{
             method:"post",
             headers:{"Content-Type" : "application/json"},
             body: JSON.stringify({
